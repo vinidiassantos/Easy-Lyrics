6edf409f46ea5c409aeb4f8071035c4cc560f8ac
Vini DiaSSantos <vinidiassantos@gmail.com>
2025-08-05 16:16:24 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "856758043115-8ktdgoa452kc6nn4gqisunpb14is7eqg.apps.googleusercontent.com",
-  authDomain: "https://vinidiassantos.github.io/Easy-Lyrics/",
+  apiKey: "AIzaSyCABDdasnNb0kCHyvW-X2g-Vct5w3TbTIk",
+  authDomain: "vini-easy-lyrics.firebaseapp.com",
   projectId: "vini-easy-lyrics",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  storageBucket: "vini-easy-lyrics.firebasestorage.app",
+  messagingSenderId: "856758043115",
+  appId: "1:856758043115:web:6acb55c36b7d632a1499ae",
+  measurementId: "G-E4705R7GFC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
-
-export { auth, provider, db };
+const analytics = getAnalytics(app);
