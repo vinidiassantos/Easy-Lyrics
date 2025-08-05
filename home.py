ad68be3c13e80cf1b647aae6dfc4d42590cb94da
Vini DiaSSantos <vinidiassantos@gmail.com>
2025-08-05 09:29:18 -0300

    Update home.py

--- home.py
+++ home.py
@@ -10,7 +10,7 @@ class CifrasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Minhas Cifras',
+      title: 'My Chords',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
